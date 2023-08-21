ece97d8c2c8fa9ba23dc913d9dfd9dd9a984a056
yohan <dygks34@gmail.com>
2023-08-22 03:18:07 +0900

    feign client 설정

--- feign/src/main/java/com/example/feign/FeignApplication.java
+++ feign/src/main/java/com/example/feign/FeignApplication.java
@@ -2,7 +2,9 @@ package com.example.feign;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
+@EnableFeignClients // feign client를 사용하기 위해 필요한 어노테이션
 @SpringBootApplication
 public class FeignApplication {
 
